db4be608bd41fedcca7619f8dbd22dd5b0ecf062
Tiago Siqueira <tiago.m.siqueira@outlook.com>
2022-08-29 22:54:21 -0300

    update routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-/* eslint linebreak-style: ["error", "windows"] */
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
-
-app.get('/', (request, response) => {
-  response.send('Hello world!');
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
